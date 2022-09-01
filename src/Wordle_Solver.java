2919f47d53bf413066df1dc4d09a6eaeaf3ebaa2
Samuel Cho <choss@wfu.edu>
2022-09-01 00:05:20 -0400

    Initial commit

---
+++ src/Wordle_Solver.java
@@ -0,0 +1,10 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Wordle_Solver {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordle~!");
+    }
+}
