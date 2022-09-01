8e5606a7adb47406eb6d6ad008a72193f70e3851
Samuel Cho <choss@wfu.edu>
2022-09-01 01:10:40 -0400

    Added colors

--- src/Wordle_Solver.java
+++ src/Wordle_Solver.java
@@ -4,7 +4,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Wordle_Solver {
+    // constants to allow colored text and backgrounds
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void main(String[] args) {
-        System.out.println("Hello Wordle~!");
+        System.out.println(ANSI_GREEN_BACKGROUND + ANSI_BLACK + "Hello" + ANSI_RESET);
+        System.out.println(ANSI_YELLOW_BACKGROUND + ANSI_BLACK + "Wordle~!" + ANSI_RESET);
     }
 }
